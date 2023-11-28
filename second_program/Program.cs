0c19a0d3e96bc811fa59f079dc83907827dae256
Erjan <erzhanasylbekuulu@gmail.com>
2023-11-28 17:35:15 +0600

    created seond program

---
+++ second_program/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// число 678
+int secondNum = num % 100 / 10;
+int thirdNum = num % 10;
+int result = 1;
+
+for (int i = 0; i < thirdNum; i++)
+{
+    result*=secondNum;
+}
+Console.WriteLine(result);
