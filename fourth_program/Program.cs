eb16c9df88a17e6b6bacea91493bff9d862ad9e6
Erjan <erzhanasylbekuulu@gmail.com>
2023-11-28 18:23:30 +0600

    fourth program created

---
+++ fourth_program/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью с конца
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 213455
+Console.WriteLine("Введите число: ");
+int integer = Convert.ToInt32(Console.ReadLine());
+if(integer/100>0){
+    Console.WriteLine((integer/100)%10);
+} else {
+    Console.WriteLine("цифры нет!");
+}
+
