2d200c337e34b31f38ea3dc4dc195c3aa65dc45f
Erjan <erzhanasylbekuulu@gmail.com>
2023-11-28 17:09:33 +0600

    first program created

---
+++ first_program/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstNumber = 0;
+int secondNumber = 0;
+int result = 0;
+
+if(number >= 100 && number <= 999){
+    firstNumber = number / 100;
+    secondNumber = number % 10;
+    result = firstNumber * 10 + secondNumber;
+
+    Console.WriteLine(result);
+
+} else {
+    Console.WriteLine("Введенное число не трехзначное!");
+}
