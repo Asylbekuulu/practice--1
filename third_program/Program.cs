2f445539415ef0b88fcaf428275e76dd014a6118
Erjan <erzhanasylbekuulu@gmail.com>
2023-11-28 17:48:59 +0600

    third program created

---
+++ third_program/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход два
+// числа и выводить, является ли второе число кратным первому.
+// Если второе число некратно первому, то программа выводит
+// остаток от деления.
+Console.WriteLine("Введите числа: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if(firstDigit % secondDigit == 0){
+    Console.WriteLine("Числа кратны!");
+} else {
+    result = firstDigit % secondDigit;
+    Console.WriteLine("Остаток от деления равен " + result);
+}
